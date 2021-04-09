922eaec2c27d8c9780bfcb92ff5ecc1e2696031f
Fernando Santos <fernandosantos511@yahoo.com.br>
2021-04-09 11:14:43 -0300

    feat: changed the behavior of search bar (should change in the future to exhibit the link to pages instead to redirect to the page)

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.wiki_page, name="page")
+    path("wiki/<str:title>", views.wiki_page, name="page"),
+    path("search/<str:query>", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,6 +5,19 @@ import re
 
 
 def index(request):
+	if request.method == 'POST':
+		querry = request.POST['q'].lower()
+
+		all_pages = util.list_entries()
+
+		for i in range(len(all_pages)):
+			if querry in all_pages[i].lower():
+				return wiki_page(request, all_pages[i])
+
+		return render(request, "encyclopedia/index.html", {
+			"entries": util.list_entries()
+		})
+
 	return render(request, "encyclopedia/index.html", {
 		"entries": util.list_entries()
 	})
@@ -17,8 +30,6 @@ def wiki_page(request, title):
 		description = util.get_entry(title.upper())
 
 	array = re.split('\n+', description)
-
-	description = description[len(title) + 2:]
 
 	array[0] = array[0].replace('#', '<h1>', 1) + '</h1>'
 
@@ -35,9 +46,42 @@ def wiki_page(request, title):
 				array[i] = array[i].replace(matches[j], f"<a href={href}> {text_link} </a>")
 			array[i] = '<p>' + array[i] + '</p>'
 
+		if "##" in array[i]:
+			array[i] = array[i].replace('##', '<h2>') + '</h2>'
+
+		if '**' in array[i]:
+			count = array[i].count('**')
+			for j in range(count):
+				if j % 2:
+					array[i] = array[i].replace('**', '</b>', 1)
+				else:
+					array[i] = array[i].replace('**', '<b>', 1)
+
+		if '*' in array[i]:
+			array[i] = array[i].replace('*', '<li>') + '</li>'
+
 	description = "\n".join(array)
 
 	return render(request, "encyclopedia/pages.html", {
 		"title": title.capitalize(),
 		"description": description,
 	})
+
+
+def search(request):
+	if request.method == 'POST':
+		query = request.POST['q']
+
+		all_pages = [entries.lower for entries in util.list_entries()]
+
+		if query.lower() in all_pages:
+			...
+
+	# 	user = authenticate(request, username=username, password=password)
+	# 	if user is not None:
+	# 		login(request, user)
+	# 		return HttpResponseRedirect(reverse('index'))
+	# 	return render(request, "users/login.html", {
+	# 		"message": 'Invalid Credentials'
+	# 	})
+	# return render(request, "users/login.html")
